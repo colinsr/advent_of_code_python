5cb80199b71f6706c4edc275f93ea002e77103f4
Colin Detwiller <colinsr@me.com>
2016-09-03 09:56:13 -0400

    day 1 part 1

---
+++ src/day_1.py
@@ -0,0 +1,4 @@
+class Day_1:
+
+    def get_directions(self, directions_as_parens):
+        return directions_as_parens.count('(') - directions_as_parens.count(')')
---
+++ test/day_1_tests.py
@@ -0,0 +1,28 @@
+import unittest
+
+from src import day_1
+
+class TestDay1(unittest.TestCase):
+
+    def test_santa_goes_up_one_floor_when_given_opening_paren(self):
+        self.assertEqual(1, day_1.Day_1().get_directions('('))
+
+    def test_santa_goes_up_two_floors_when_given_two_opening_parens(self):
+        self.assertEqual(2, day_1.Day_1().get_directions('(('))
+
+    def test_santa_goes_down_one_floor_when_given_closing_paren(self):
+        self.assertEqual(-1, day_1.Day_1().get_directions(')'))
+
+    def test_santa_goes_down_two_floors_when_given_two_closing_parens(self):
+        self.assertEqual(-2, day_1.Day_1().get_directions('))'))
+
+    def test_santa_finished_on_floor_zero_when_given_equal_opening_and_closing_parens(self):
+        'test going up one floor'
+        self.assertEqual(0, day_1.Day_1().get_directions('()()'))
+
+    def test_santa_finished_on_first_basementfloor_zero_when_given_two_closing_and_one_opening_paren(self):
+        'test going up one floor'
+        self.assertEqual(-1, day_1.Day_1().get_directions('))('))
+
+if __name__ == '__main__':
+    unittest.main()
